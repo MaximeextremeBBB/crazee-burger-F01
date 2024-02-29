8ada9d9ac73244e6db4b92b187c6c57648d88e2f
MaximeextremeBBB <maxime.guiraud.1990@gmail.com>
2024-02-29 11:10:03 +0100

    ✨ F01: CHANGE > Create LoginPage

--- src/App.js
+++ src/App.js
@@ -1,51 +1,10 @@
 import { useState } from "react";
 import logo from "./logo.svg";
+import LoginPage from "./components/pages/LoginPage";
 // import "./App.css";
 
 function App() {
-    const [message, setMessage] = useState("");
-    const handleChange = (event) => {
-        setMessage(event.target.value);
-        console.log(event.target.value);
-    };
-
-    const handleClick = (event) => {
-        if (message != "") {
-            alert("Bonjour" + message);
-        } else {
-            alert("Veuillez rentrez un nom s'il vous plaît");
-        }
-    };
-
-    return (
-        <div className="App">
-            <header className="App-header">
-                <div>
-                    <p>
-                        {" "}
-                        <h1>Bienvenue chez nous !</h1>
-                    </p>
-                </div>
-                <div className="login">
-                    <div>
-                        <h3>Connectez-vous</h3>
-                    </div>
-                </div>
-                <form action="">
-                    <input
-                        type="text"
-                        onChange={handleChange}
-                        placeholder="Entrez votre prénom"
-                    />
-                    <input
-                        type="submit"
-                        onClick={handleClick}
-                        value="Accédez à votre espace"
-                    />
-                </form>
-            </header>
-        </div>
-    );
+    return <LoginPage />;
 }
 
 export default App;
--- src/components/pages/LoginPage.js
+++ src/components/pages/LoginPage.js
@@ -13,7 +13,49 @@
 import React from "react";
 
 const LoginPage = () => {
-    return <div>LoginPage</div>;
+    const [message, setMessage] = useState("");
+    const handleChange = (event) => {
+        setMessage(event.target.value);
+        console.log(event.target.value);
+    };
+
+    const handleClick = (event) => {
+        if (message != "") {
+            alert("Bonjour" + message);
+        } else {
+            alert("Veuillez rentrez un nom s'il vous plaît");
+        }
+    };
+
+    return (
+        <div className="App">
+            <header className="App-header">
+                <div>
+                    <p>
+                        {" "}
+                        <h1>Bienvenue chez nous !</h1>
+                    </p>
+                </div>
+                <div className="login">
+                    <div>
+                        <h3>Connectez-vous</h3>
+                    </div>
+                </div>
+                <form action="">
+                    <input
+                        type="text"
+                        onChange={handleChange}
+                        placeholder="Entrez votre prénom"
+                    />
+                    <input
+                        type="submit"
+                        onClick={handleClick}
+                        value="Accédez à votre espace"
+                    />
+                </form>
+            </header>
+        </div>
+    );
 };
 
 export default LoginPage;
