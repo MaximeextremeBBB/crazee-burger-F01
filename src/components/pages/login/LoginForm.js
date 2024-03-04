d7d43d6074b90258f8f9fbeab8ed52b8c0cbf736
MaximeextremeBBB <maxime.guiraud.1990@gmail.com>
2024-03-04 11:27:08 +0100

    💄 F03-live-AMA method to add styles

--- src/components/pages/login/LoginForm.js
+++ src/components/pages/login/LoginForm.js
@@ -2,10 +2,16 @@ import React, { useContext, useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import { NameContext } from "../../../App";
 import OrderPage from "../order/OrderPage";
+import "./LoginForm.css";
 
 const LoginForm = () => {
     const [inputValue, setInputValue] = useState("");
     const navigate = useNavigate();
+    const titreH2style = {
+        backgroundColor: "blue",
+        color: "white",
+        fontSize: 25,
+    };
 
     // test
 
@@ -33,15 +39,18 @@ const LoginForm = () => {
         // {/* <form action=""> */}
 
         <form action="submit" onSubmit={handleSubmit}>
-            <h1>Bienvenue chez nous !</h1>
+            <h1 style={{ color: "red" }}>Bienvenue chez nous !</h1>
+            {/* F03 présentation du ticket 17:35s Après le mot style il y a yne accolade c'est une interpolation jsx et donc on quitte le monde du jsx et on va dans du js la deuxième accolade , c'est pck c'est un objett js */}
+            {/* F03 présentation du ticket 19:20s ne pas oublier les guillemets à red car c'est un objet js */}
             <br />
-            <h2>Connectez-vous</h2>
+            <h2 style={titreH2style}>Connectez-vous</h2>
             <input
                 value={inputValue}
                 type="text"
                 onChange={handleChange}
                 placeholder="Entrez votre prénom..."
                 required
+                className="bonbon"
             />
             <input
                 type="submit"
