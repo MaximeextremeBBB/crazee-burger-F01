354a8b627129a0792b92458e34a03685ce4d4a6f
MaximeextremeBBB <maxime.guiraud.1990@gmail.com>
2024-03-08 14:10:05 +0100

    ✨ F03: Login > Add background

--- src/components/pages/login/LoginForm.js
+++ src/components/pages/login/LoginForm.js
@@ -5,6 +5,8 @@ import { NameContext } from "../../../App";
 import OrderPage from "../order/OrderPage";
 import "./LoginForm.css";
 import { theme } from "../../../theme";
+import burgerBg from "../../../../src/assets/F03 burger-background.jpg";
+import logoOrange from "../../../../src/assets/F03 logo-orange.png";
 
 const LoginForm = () => {
     const [inputValue, setInputValue] = useState("");
@@ -37,12 +39,50 @@ const LoginForm = () => {
             alert("Veuillez rentrez un nom s'il vous plaît");
         }
     };
-    return (
-        // {/* <form action=""> */}
+    // return (
+    //     // {/* <form action=""> */}
 
+    //     <form action="submit" onSubmit={handleSubmit}>
+    //         <div className="container-text">
+    //             <h1 style={{ color: "red" }}>Bienvenue chez nous !</h1>
+    //             {/* F03 présentation du ticket 17:35s Après le mot style il y a yne accolade c'est une interpolation jsx et donc on quitte le monde du jsx et on va dans du js la deuxième accolade , c'est pck c'est un objett js */}
+    //             {/* F03 présentation du ticket 19:20s ne pas oublier les guillemets à red car c'est un objet js */}
+    //             <br />
+    //             <h2 style={titreH2style}>Connectez-vous</h2>
+    //         </div>
+    //         <input
+    //             value={inputValue}
+    //             type="text"
+    //             onChange={handleChange}
+    //             placeholder="Entrez votre prénom"
+    //             required
+    //             className="bonbon"
+    //         />
+    //         <input
+    //             type="submit"
+    //             // onClick={handleClick}
+    //             value="Accédez à mon espace"
+    //         />
+    //         <br />
+    //         {/* <a href="/order">Vers order page (anchor link)</a>{" "} */}
+    //         {/*//l'inconvénient de cette méthode c'est qu'elle refresh la page avec le Link on a pas ce PropTypes.bool */}
+    //         {/* <br /> */}
+    //         {/* <Link to="/order">vers order page (Link)</Link> */}
+    //     </form>
+    // );
+    return (
         <LoginFormStyled action="submit" onSubmit={handleSubmit}>
             <div className="container-text">
+                {/* <TitleBackground> */}
+                <TitleStyled>
+                    <div className="left-part">CRAZEE</div>
+                    <div className="middle-part">
+                        <img src="${logoOrange}" alt="" />
+                    </div>
+                    <div className="right-part">BURGER</div>
+                </TitleStyled>
                 <h1 style={{ color: "red" }}>Bienvenue chez nous !</h1>
+                {/* </TitleBackground> */}
                 {/* F03 présentation du ticket 17:35s Après le mot style il y a yne accolade c'est une interpolation jsx et donc on quitte le monde du jsx et on va dans du js la deuxième accolade , c'est pck c'est un objett js */}
                 {/* F03 présentation du ticket 19:20s ne pas oublier les guillemets à red car c'est un objet js */}
                 <br />
@@ -52,14 +92,15 @@ const LoginForm = () => {
                 value={inputValue}
                 type="text"
                 onChange={handleChange}
-                placeholder="Entrez votre prénom..."
+                placeholder="Entrez votre prénom"
                 required
                 className="bonbon"
             />
+            <br />
             <input
                 type="submit"
                 // onClick={handleClick}
-                value="Accédez à votre espace"
+                value="Accédez à mon espace >"
             />
             <br />
             {/* <a href="/order">Vers order page (anchor link)</a>{" "} */}
@@ -70,13 +111,49 @@ const LoginForm = () => {
     );
 };
 
+// DEBUT DU COURS
+// const LoginFormStyled = styled.form`
+//     border: 1px solid red;
+//     background: ${theme.colors.primary};
+//     h1 {
+//         font-size: 100px;
+//     }
+// `;
+// // F03 live ama 47:06 styled est un objet qui lui en propriété il va avoir toutes les balises qui existes en HTML et jsx on peut mettre une div ou un lien ou un boton ou ect
+// FIN DU COURS
+
+// const TitleBackground = styled.div`
+//     height: 1000px;
+//     width: 100%;
+// `;
+
 const LoginFormStyled = styled.form`
-    border: 1px solid red;
-    background: ${theme.colors.primary};
-    h1 {
-        font-size: 100px;
+    background: url("${burgerBg}") no-repeat center/cover;
+    height: 100vh;
+    text-align: center;
+`;
+
+const TitleStyled = styled.div`
+    display: flex;
+    justify-content: center;
+    color: orange;
+    font-size: 50px;
+    padding: 150px;
+    .left-part {
+        width: 45%;
+        text-align: right;
+    }
+    .middle-part {
+        background: url("${logoOrange}") no-repeat center;
+        height: 100px;
+        background-size: 100% 100%;
+        width: 10%;
+    }
+    .right-part {
+        width: 45%;
+        text-align: left;
     }
 `;
-// F03 live ama 47:06 styled est un objet qui lui en propriété il va avoir toutes les balises qui existes en HTML et jsx on peut mettre une div ou un lien ou un boton ou ect
+const logoOrangeStyled = styled.p``;
 
 export default LoginForm;
