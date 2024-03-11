87e80a4cbb9aa1949bd31b1dcf767f2ee820f5d3
MaximeextremeBBB <maxime.guiraud.1990@gmail.com>
2024-03-11 14:20:21 +0100

    ✨F03: loginForm CSS >  mon code final

--- src/components/pages/login/LoginForm.js
+++ src/components/pages/login/LoginForm.js
@@ -9,6 +9,7 @@ import burgerBg from "../../../../src/assets/F03 burger-background.jpg";
 import logoOrange from "../../../../src/assets/F03 logo-orange.png";
 import amaticSC from "../../../../src/assets/fonts/AmaticSC-Regular.ttf";
 import openSans from "../../../../src/assets/fonts/OpenSans_Condensed-Medium.ttf";
+import { FaRegUserCircle } from "react-icons/fa";
 
 const LoginForm = () => {
     const [inputValue, setInputValue] = useState("");
@@ -75,36 +76,46 @@ const LoginForm = () => {
     // );
     return (
         <LoginFormStyled action="submit" onSubmit={handleSubmit}>
-            <div className="container-text">
-                {/* <TitleBackground> */}
-                <TitleStyled>
-                    <div className="left-part">CRAZEE</div>
-                    <div className="middle-part">
-                        <img src="${logoOrange}" alt="" />
-                    </div>
-                    <div className="right-part">BURGER</div>
-                </TitleStyled>
+            {/* <div className="container-text"> */}
+            {/* <TitleBackground> */}
+            <TitleStyled>
+                <div className="left-part">CRAZEE</div>
+                <div className="middle-part">
+                    <img src="${logoOrange}" alt="" />
+                </div>
+                <div className="right-part">BURGER</div>
+            </TitleStyled>
+            <ContentStyled>
                 <h1>Bienvenue chez nous !</h1>
                 {/* </TitleBackground> */}
                 {/* F03 présentation du ticket 17:35s Après le mot style il y a yne accolade c'est une interpolation jsx et donc on quitte le monde du jsx et on va dans du js la deuxième accolade , c'est pck c'est un objett js */}
                 {/* F03 présentation du ticket 19:20s ne pas oublier les guillemets à red car c'est un objet js */}
                 <br />
                 <h2 style={titreH2style}>Connectez-vous</h2>
-            </div>
-            <input
-                value={inputValue}
-                type="text"
-                onChange={handleChange}
-                placeholder="Entrez votre prénom"
-                required
-                className="bonbon"
-            />
+            </ContentStyled>
+            {/* </div> */}
+            <InputStyled>
+                <div className="userCircle">
+                    <FaRegUserCircle />
+                </div>
+                <input
+                    value={inputValue}
+                    type="text"
+                    onChange={handleChange}
+                    placeholder="Entrez votre prénom"
+                    required
+                    className="bonbon"
+                />
+            </InputStyled>
+
             <br />
-            <input
-                type="submit"
-                // onClick={handleClick}
-                value="Accédez à mon espace >"
-            />
+            <InputSubmitStyled>
+                <input
+                    type="submit"
+                    // onClick={handleClick}
+                    value="Accédez à mon espace >"
+                />
+            </InputSubmitStyled>
             <br />
             {/* <a href="/order">Vers order page (anchor link)</a>{" "} */}
             {/*//l'inconvénient de cette méthode c'est qu'elle refresh la page avec le Link on a pas ce PropTypes.bool */}
@@ -134,27 +145,13 @@ const LoginFormStyled = styled.form`
     background: url("${burgerBg}") no-repeat center/cover;
     height: 100vh;
     text-align: center;
+    display: block;
+    margin: 0 auto;
     @font-face {
         font-family: "openSans";
         src: url("${openSans}");
     }
     font-family: openSans, sans-serif;
-    h1 {
-        color: white;
-        position: relative;
-        display: block;
-        margin: 50px auto;
-        width: 30%;
-        &:before {
-            position: absolute;
-            height: 1px;
-            content: "";
-            background: orange;
-            width: 100%;
-            margin: 0 -40px;
-            transform: translateY(120px);
-        }
-    }
 `;
 
 const TitleStyled = styled.div`
@@ -187,9 +184,57 @@ const TitleStyled = styled.div`
         text-align: left;
     }
 `;
-const hunStyled = styled.h1`
-    color: white;
-    background: red;
+
+const ContentStyled = styled.div`
+    position: relative;
+    h1 {
+        color: white;
+        /* position: relative; */
+        display: block;
+        margin: 50px auto;
+        &:before {
+            position: absolute;
+            height: 1px;
+            content: "";
+            background: orange;
+            width: 20%;
+            margin: 0 -20px;
+            transform: translateY(120px);
+        }
+    }
+`;
+
+const InputStyled = styled.div`
+    border: 1px solid black;
+    background: white;
+    display: flex;
+    justify-content: center;
+    margin: 0 auto;
+    width: 20%;
+    border-radius: 5px;
+    padding: 10px 0;
+    .userCircle {
+        margin: 0 10px;
+    }
+    input {
+        width: 90%;
+        border: none;
+    }
+`;
+
+const InputSubmitStyled = styled.div`
+    background: white;
+    border: 1px solid orange;
+    margin: 0 auto;
+    padding: 10px 0;
+    width: 20%;
+    cursor: pointer;
+    input {
+        background: white;
+        border: none;
+        color: orange;
+        border-radius: 5px;
+    }
 `;
 
 export default LoginForm;
