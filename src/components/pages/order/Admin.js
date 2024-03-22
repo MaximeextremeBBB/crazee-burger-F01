5ed9151ed1658d96bb3b6d5f9c86a725e1875f9e
MaximeextremeBBB <maxime.guiraud.1990@gmail.com>
2024-03-22 11:30:05 +0100

    ✨ ORDER > ADMIN > Add Add-Product and add the div missing

--- src/components/pages/order/Admin.js
+++ src/components/pages/order/Admin.js
@@ -1,15 +1,24 @@
 import React from "react";
 import styled from "styled-components/macro";
 import { theme } from "../../../theme";
-import { IoChevronForward } from "react-icons/io5";
+import { IoChevronDown } from "react-icons/io5";
+import { AiOutlinePlus } from "react-icons/ai";
 
 export default function Admin() {
     return (
         <AdminStyled>
             Admin
-            <TestPosition>
-                <IoChevronForward />
-            </TestPosition>
+            <IconsPosition>
+                <ChevronUpPositionStyled>
+                    <IoChevronDown />
+                </ChevronUpPositionStyled>
+                <AddProductStyled>
+                    <div>
+                        <AiOutlinePlus />
+                    </div>
+                    <div>Ajouter un Produit</div>
+                </AddProductStyled>
+            </IconsPosition>
         </AdminStyled>
     );
 }
@@ -20,18 +29,38 @@ const AdminStyled = styled.div`
     position: relative;
 `;
 
-const TestPosition = styled.div`
-    content: "";
+const IconsPosition = styled.div`
     position: absolute;
-    width: 60px;
-    height: 43px;
-    background: ${theme.colors.white};
+    width: 100%;
+    height: 44px;
     top: -46px;
-    left: 71px;
+    display: flex;
+    align-items: center;
+`;
+
+const ChevronUpPositionStyled = styled.div`
+    margin-left: 71px;
+    height: 43px;
+    width: 60px;
     display: flex;
     align-items: center;
     justify-content: center;
+
+    background: ${theme.colors.white};
     color: ${theme.colors.greyLight};
     border: 1px solid ${theme.colors.greyLight};
     border-radius: 5px 5px 0px 0px;
+`;
+
+const AddProductStyled = styled.div`
+    width: 212.02px;
+    height: 43px;
+    background: ${theme.colors.background_dark};
+    top: -46px;
+    left: 171px;
+    color: ${theme.colors.white};
+
+    display: flex;
+    justify-content: center;
+    align-items: center;
 `;
