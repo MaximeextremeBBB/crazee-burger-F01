4a31ff68ae27331f1e8cdf6c19f8076763ca41d6
MaximeextremeBBB <maxime.guiraud.1990@gmail.com>
2024-03-22 10:49:20 +0100

    ✨ F07 : ORDER >  ADMIN > Add the first icon above the Admin menu

--- src/components/pages/order/Admin.js
+++ src/components/pages/order/Admin.js
@@ -1,10 +1,37 @@
 import React from "react";
 import styled from "styled-components/macro";
+import { theme } from "../../../theme";
+import { IoChevronForward } from "react-icons/io5";
+
 export default function Admin() {
-    return <AdminStyled>Admin</AdminStyled>;
+    return (
+        <AdminStyled>
+            Admin
+            <TestPosition>
+                <IoChevronForward />
+            </TestPosition>
+        </AdminStyled>
+    );
 }
 
 const AdminStyled = styled.div`
     background: red fixed;
     height: 100px;
+    position: relative;
+`;
+
+const TestPosition = styled.div`
+    content: "";
+    position: absolute;
+    width: 60px;
+    height: 43px;
+    background: ${theme.colors.white};
+    top: -46px;
+    left: 71px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    color: ${theme.colors.greyLight};
+    border: 1px solid ${theme.colors.greyLight};
+    border-radius: 5px 5px 0px 0px;
 `;
