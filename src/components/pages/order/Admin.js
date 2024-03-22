f581f5d41d533f8d1fd0c0a60816882db1522ba8
MaximeextremeBBB <maxime.guiraud.1990@gmail.com>
2024-03-22 09:59:22 +0100

    ✨ F07 : ORDER > Creation of the bottom bar and make it fixed

---
+++ src/components/pages/order/Admin.js
@@ -0,0 +1,10 @@
+import React from "react";
+import styled from "styled-components/macro";
+export default function Admin() {
+    return <AdminStyled>Admin</AdminStyled>;
+}
+
+const AdminStyled = styled.div`
+    background: red fixed;
+    height: 100px;
+`;
