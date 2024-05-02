8e244fba02c920d16cb9125b1801f8da416e9823
MaximeextremeBBB <maxime.guiraud.1990@gmail.com>
2024-05-02 16:31:13 +0200

    ✨ F12 EDITFORM > When change a product in menu it will change product in basket

--- src/components/pages/order/Main/MainRightSide/Admin/AdminPanel/EditForm.js
+++ src/components/pages/order/Main/MainRightSide/Admin/AdminPanel/EditForm.js
@@ -1,29 +1,50 @@
-import { useContext } from "react"
-import OrderContext from "../../../../../../../context/OrderContext"
-import Form from "./Form"
-import EditInfoMessage from "./EditInfoMessage"
+import { useContext } from "react";
+import OrderContext from "../../../../../../../context/OrderContext";
+import Form from "./Form";
+import EditInfoMessage from "./EditInfoMessage";
+import { find } from "../../../../../../../utils/array";
 
 export default function EditForm() {
-  // state
-  const { productSelected, setProductSelected, handleEdit, titleEditRef } = useContext(OrderContext)
+    // state
+    const {
+        productSelected,
+        setProductSelected,
+        handleEdit,
+        basket,
+        handleEditBasketAndMenu,
+        titleEditRef,
+    } = useContext(OrderContext);
 
-  // comportements (gestionnaires d'événement ou "event handlers")
-  const handleChange = (event) => {
-    const { name, value } = event.target
+    // comportements (gestionnaires d'événement ou "event handlers")
+    const handleChange = (event) => {
+        const { name, value } = event.target;
 
-    const productBeingUpdated = {
-      ...productSelected,
-      [name]: value,
-    }
+        const productBeingUpdated = {
+            ...productSelected,
+            [name]: value,
+        };
+        const isProductAlreadyInBasket =
+            find(productBeingUpdated.id, basket) !== undefined;
 
-    setProductSelected(productBeingUpdated) // cette ligne update le formulaire
-    handleEdit(productBeingUpdated, event) // cette ligne update le menu
-  }
+        setProductSelected(productBeingUpdated); // cette ligne update le formulaire
+        handleEdit(productBeingUpdated, event); // cette ligne update le menu
 
-  // affichage
-  return (
-    <Form product={productSelected} onChange={handleChange} ref={titleEditRef}>
-      <EditInfoMessage />
-    </Form>
-  )
+        console.log(productBeingUpdated);
+        console.log(isProductAlreadyInBasket);
+
+        if (isProductAlreadyInBasket) {
+            handleEditBasketAndMenu(productBeingUpdated);
+        }
+    };
+
+    // affichage
+    return (
+        <Form
+            product={productSelected}
+            onChange={handleChange}
+            ref={titleEditRef}
+        >
+            <EditInfoMessage />
+        </Form>
+    );
 }
