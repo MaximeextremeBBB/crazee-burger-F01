670932ab90216a80b77abf73edbef8c42825c17e
MaximeextremeBBB <maxime.guiraud.1990@gmail.com>
2024-05-02 15:20:19 +0200

    ♻️   F11 Final code for the F11

--- src/components/pages/order/Main/Basket/Basket.js
+++ src/components/pages/order/Main/Basket/Basket.js
@@ -1,27 +1,56 @@
-import styled from "styled-components";
-import Total from "./Total";
-import { formatPrice } from "../../../../../utils/maths";
-import Footer from "./Footer";
-import BasketBody from "./BasketBody";
-import MainContext from "../../../../../context/MainContext";
-import { useContext } from "react";
+import styled from "styled-components"
+import Total from "./Total"
+import { formatPrice } from "../../../../../utils/maths"
+import Footer from "./Footer"
+import EmptyBasket from "./EmptyBasket"
+import { useContext } from "react"
+import OrderContext from "../../../../../context/OrderContext"
+import BasketProducts from "./BasketProducts"
+import { theme } from "../../../../../theme"
 
 export default function Basket() {
-    const { newProductBasket, totalPrice } = useContext(MainContext);
-    const newPrice = newProductBasket.price;
+  const { basket, isModeAdmin, handleDeleteBasketProduct } = useContext(OrderContext)
 
-    return (
-        <BasketStyled>
-            <Total amountToPay={formatPrice(totalPrice)} />
-            <BasketBody />
-            <Footer />
-        </BasketStyled>
-    );
+  const isBasketEmpty = basket.length === 0
+
+  const sumToPay = basket.reduce((total, basketProduct) => {
+    total += basketProduct.price * basketProduct.quantity
+    return total
+  }, 0)
+
+  return (
+    <BasketStyled>
+      <Total amountToPay={formatPrice(sumToPay)} />
+      {isBasketEmpty ? (
+        <EmptyBasket />
+      ) : (
+        <BasketProducts
+          basket={basket}
+          isModeAdmin={isModeAdmin}
+          handleDeleteBasketProduct={handleDeleteBasketProduct}
+        />
+      )}
+      <Footer />
+    </BasketStyled>
+  )
 }
 
 const BasketStyled = styled.div`
-    background: pink;
-    display: flex;
-    flex-direction: column;
-    overflow-y: scroll;
-`;
+  background: ${theme.colors.background_white};
+  box-shadow: ${theme.shadows.basket};
+  display: flex;
+  flex-direction: column;
+  border-bottom-left-radius: ${theme.borderRadius.extraRound};
+  height: 85vh;
+
+  .head {
+    position: sticky;
+    top: 0;
+  }
+
+  .footer {
+    border-bottom-left-radius: ${theme.borderRadius.extraRound};
+    position: sticky;
+    bottom: 0;
+  }
+`
