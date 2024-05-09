9ea449e25f588c4d17b5cbced5c9eaaae9b0111a
MaximeextremeBBB <maxime.guiraud.1990@gmail.com>
2024-05-09 17:04:43 +0200

    🚀  F13 : Final code

--- src/components/pages/order/Main/MainRightSide/Menu/Menu.js
+++ src/components/pages/order/Main/MainRightSide/Menu/Menu.js
@@ -1,109 +1,82 @@
-import { useContext } from "react";
-import styled from "styled-components";
-import OrderContext from "../../../../../../context/OrderContext";
-import { theme } from "../../../../../../theme";
-import { formatPrice } from "../../../../../../utils/maths";
-import Card from "../../../../../reusable-ui/Card";
-import EmptyMenuAdmin from "./EmptyMenuAdmin";
-import EmptyMenuClient from "./EmptyMenuClient";
-import { checkIfProductIsClicked } from "./helper";
-import {
-    EMPTY_PRODUCT,
-    IMAGE_COMING_SOON,
-} from "../../../../../../enums/product";
-import { isEmpty } from "../../../../../../utils/array";
-import { useParams } from "react-router-dom";
-import { getUser } from "../../../../../../api/user";
-import { useEffect } from "react";
+import { useContext } from "react"
+import styled from "styled-components"
+import OrderContext from "../../../../../../context/OrderContext"
+import { theme } from "../../../../../../theme"
+import { formatPrice } from "../../../../../../utils/maths"
+import Card from "../../../../../reusable-ui/Card"
+import EmptyMenuAdmin from "./EmptyMenuAdmin"
+import EmptyMenuClient from "./EmptyMenuClient"
+import { checkIfProductIsClicked } from "./helper"
+import { EMPTY_PRODUCT, IMAGE_COMING_SOON } from "../../../../../../enums/product"
+import { isEmpty } from "../../../../../../utils/array"
+import Loader from "./Loader"
 
 export default function Menu() {
-    // export const Menu = async () => {
-    const {
-        menu,
-        isModeAdmin,
-        handleDelete,
-        resetMenu,
-        productSelected,
-        setProductSelected,
-        handleAddToBasket,
-        handleDeleteBasketProduct,
-        handleProductSelected,
-    } = useContext(OrderContext);
-    const { username } = useParams();
+  const {
+    username,
+    menu,
+    isModeAdmin,
+    handleDelete,
+    resetMenu,
+    productSelected,
+    setProductSelected,
+    handleAddToBasket,
+    handleDeleteBasketProduct,
+    handleProductSelected,
+  } = useContext(OrderContext)
+  // state
 
-    // state
+  // comportements (gestionnaires d'événement ou "event handlers")
+  const handleCardDelete = (event, idProductToDelete) => {
+    event.stopPropagation()
+    handleDelete(idProductToDelete, username)
+    handleDeleteBasketProduct(idProductToDelete, username)
+    idProductToDelete === productSelected.id && setProductSelected(EMPTY_PRODUCT)
+  }
 
-    // comportements (gestionnaires d'événement ou "event handlers")
-    const handleCardDelete = (event, idProductToDelete) => {
-        event.stopPropagation();
-        handleDelete(idProductToDelete);
-        handleDeleteBasketProduct(idProductToDelete);
-        idProductToDelete === productSelected.id &&
-            setProductSelected(EMPTY_PRODUCT);
-    };
+  const handleAddButton = (event, idProductToAdd) => {
+    event.stopPropagation()
+    handleAddToBasket(idProductToAdd, username)
+  }
 
-    const handleAddButton = (event, idProductToAdd) => {
-        event.stopPropagation();
-        handleAddToBasket(idProductToAdd);
-    };
+  // affichage
+  if (menu === undefined) return <Loader />
 
-    // const activateUser = async (username) => {
-    //     // const existingUser = await getUser(username);
-    //     // console.log("existingUser : ", existingUser.menu);
-    //     // return existingUser.menu;
-    //     return "bou";
-    // };
-    // const tab = activateUser(username);
+  if (isEmpty(menu)) {
+    if (!isModeAdmin) return <EmptyMenuClient />
+    return <EmptyMenuAdmin onReset={() => resetMenu(username)} />
+  }
 
-    useEffect(async () => {
-        const menuReceived = await getUser(username);
-        console.log("menuReceived : ", menuReceived);
-        // setMenu(menuReceived);
-    }, []);
-
-    // affichage
-    // if (isEmpty(getUser(username))) {
-    //     if (!isModeAdmin) return <EmptyMenuClient />;
-    //     return <EmptyMenuAdmin onReset={resetMenu} />;
-    // }
-
-    return (
-        <MenuStyled className="menu">
-            {menu.map(({ id, title, imageSource, price }) => {
-                return (
-                    <Card
-                        key={id}
-                        title={title}
-                        imageSource={
-                            imageSource ? imageSource : IMAGE_COMING_SOON
-                        }
-                        leftDescription={formatPrice(price)}
-                        hasDeleteButton={isModeAdmin}
-                        onDelete={(event) => handleCardDelete(event, id)}
-                        onClick={
-                            isModeAdmin ? () => handleProductSelected(id) : null
-                        }
-                        isHoverable={isModeAdmin}
-                        isSelected={checkIfProductIsClicked(
-                            id,
-                            productSelected.id
-                        )}
-                        onAdd={(event) => handleAddButton(event, id)}
-                    />
-                );
-            })}
-        </MenuStyled>
-    );
+  return (
+    <MenuStyled className="menu">
+      {menu.map(({ id, title, imageSource, price }) => {
+        return (
+          <Card
+            key={id}
+            title={title}
+            imageSource={imageSource ? imageSource : IMAGE_COMING_SOON}
+            leftDescription={formatPrice(price)}
+            hasDeleteButton={isModeAdmin}
+            onDelete={(event) => handleCardDelete(event, id)}
+            onClick={isModeAdmin ? () => handleProductSelected(id) : null}
+            isHoverable={isModeAdmin}
+            isSelected={checkIfProductIsClicked(id, productSelected.id)}
+            onAdd={(event) => handleAddButton(event, id)}
+          />
+        )
+      })}
+    </MenuStyled>
+  )
 }
 
 const MenuStyled = styled.div`
-    background: ${theme.colors.background_white};
-    display: grid;
-    grid-template-columns: repeat(3, 1fr);
-    /* grid-template-columns: repeat(auto-fit, minmax(300px, 1fr)); */
-    grid-row-gap: 60px;
-    padding: 50px 50px 150px;
-    justify-items: center;
-    box-shadow: 0px 8px 20px 8px rgba(0, 0, 0, 0.2) inset;
-    overflow-y: scroll;
-`;
+  background: ${theme.colors.background_white};
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
+  /* grid-template-columns: repeat(auto-fit, minmax(300px, 1fr)); */
+  grid-row-gap: 60px;
+  padding: 50px 50px 150px;
+  justify-items: center;
+  box-shadow: 0px 8px 20px 8px rgba(0, 0, 0, 0.2) inset;
+  overflow-y: scroll;
+`
