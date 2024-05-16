b5a80a19d41a2ebf0efbde76f49ddf8812d13297
MaximeextremeBBB <maxime.guiraud.1990@gmail.com>
2024-05-16 15:47:09 +0200

    📦 F14 : BASKET > Total > Make both elementd visible on props change

--- src/components/reusable-ui/CasinoEffect.js
+++ src/components/reusable-ui/CasinoEffect.js
@@ -2,8 +2,28 @@ import React from "react";
 
 export default function CasinoEffect({ count, className }) {
     return (
-        <div>
-            <span className={className}>{count}</span>
-        </div>
+        <TransitionGroup component={CasinoEffectStyled}>
+            <CSSTransition
+                classNames={"count-animated"}
+                timeout={5000}
+                key={count}
+            >
+                <span className={className}>{count}</span>
+            </CSSTransition>
+        </TransitionGroup>
     );
 }
+
+const CasinoEffectStyled = styled.div`
+    border: 1px solid red;
+    count-animated {
+    }
+    count-animated-enter-active {
+    }
+    count-animated-enter-done {
+    }
+    count-animated-exit-active {
+    }
+    count-animated-exit-done {
+    }
+`;
