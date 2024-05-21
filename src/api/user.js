fadde2246934dfa3832337ba86689e4c9fa852ad
MaximeextremeBBB <maxime.guiraud.1990@gmail.com>
2024-05-21 11:40:36 +0200

    🚀   F14 : Final Code

--- src/api/user.js
+++ src/api/user.js
@@ -1,28 +1,41 @@
-import { doc, getDoc, setDoc } from "firebase/firestore";
-import { db } from "./firebase-config";
-import { fakeMenu } from "../fakeData/fakeMenu";
+import { doc, getDoc, setDoc } from "firebase/firestore"
+import { db } from "./firebase-config"
+import { fakeMenu } from "../fakeData/fakeMenu"
 
 export const getUser = async (idUser) => {
-    const cachette = doc(db, "users", idUser);
-    const docSnapshot = await getDoc(cachette);
-    // console.log("docSnapshot : ", docSnapshot);
-    // console.log("docSnapshot.exists() : ", docSnapshot.exists());
+  //const docRef = doc(CHEMIN)
+  const docRef = doc(db, "users", idUser)
 
-    if (docSnapshot.exists()) {
-        const { menu } = await docSnapshot.data();
-        console.log("menu : ", menu);
-        return menu;
-    }
-};
+  const docSnapshot = await getDoc(docRef)
+  if (docSnapshot.exists()) {
+    const userReceived = docSnapshot.data()
+    return userReceived
+  }
+}
+
+// Quand une fonction retourne une promesse, cette promesse ne peut avoir que 3 valeurs possibles :
+// 1er cas : promesse en cours d'achèvement => Promise (pending)
+// 2e cas : résultat positif de la promesse achevée => résultat positif (fulfilled)
+// 3e cas : résultat négatif de la promesse achevée => résultat négatif (rejected)
 
 export const createUser = (userId) => {
-    // CACHETTE
-    const cachette = doc(db, "users", userId);
+  // CACHETTE
+  const docRef = doc(db, "users", userId)
 
-    // NOURRITURE
-    const nourriture = {
-        username: userId,
-        menu: fakeMenu.LARGE,
-    };
-    setDoc(cachette, nourriture);
-};
+  // NOURRITURE
+  const nourriture = {
+    username: userId,
+    menu: fakeMenu.SMALL,
+  }
+
+  //setDoc(CACHETTE, NOURRITURE)
+  setDoc(docRef, nourriture)
+}
+
+export const authenticateUser = async (userId) => {
+  const existingUser = await getUser(userId)
+
+  if (!existingUser) {
+    createUser(userId)
+  }
+}
--- src/components/pages/order/Main/MainRightSide/Menu/Menu.js
+++ src/components/pages/order/Main/MainRightSide/Menu/Menu.js
@@ -1,150 +1,87 @@
-import { useContext } from "react";
-import styled from "styled-components";
-import OrderContext from "../../../../../../context/OrderContext";
-import { theme } from "../../../../../../theme";
-import { formatPrice } from "../../../../../../utils/maths";
-import Card from "../../../../../reusable-ui/Card";
-import EmptyMenuAdmin from "./EmptyMenuAdmin";
-import EmptyMenuClient from "./EmptyMenuClient";
-import { checkIfProductIsClicked } from "./helper";
-import {
-    EMPTY_PRODUCT,
-    IMAGE_COMING_SOON,
-} from "../../../../../../enums/product";
-import { isEmpty } from "../../../../../../utils/array";
-import { useParams } from "react-router-dom";
-import { getUser } from "../../../../../../api/user";
-import { useEffect } from "react";
-import { TransitionGroup, CSSTransition } from "react-transition-group";
+import { useContext } from "react"
+import styled from "styled-components"
+import OrderContext from "../../../../../../context/OrderContext"
+import { theme } from "../../../../../../theme"
+import { formatPrice } from "../../../../../../utils/maths"
+import Card from "../../../../../reusable-ui/Card"
+import EmptyMenuAdmin from "./EmptyMenuAdmin"
+import EmptyMenuClient from "./EmptyMenuClient"
+import { checkIfProductIsClicked } from "./helper"
+import { EMPTY_PRODUCT, IMAGE_COMING_SOON } from "../../../../../../enums/product"
+import { isEmpty } from "../../../../../../utils/array"
+import Loader from "./Loader"
+import { CSSTransition, TransitionGroup } from "react-transition-group"
+import { menuAnimation } from "../../../../../../theme/animations"
 
 export default function Menu() {
-    // export const Menu = async () => {
-    const {
-        menu,
-        isModeAdmin,
-        handleDelete,
-        resetMenu,
-        productSelected,
-        setProductSelected,
-        handleAddToBasket,
-        handleDeleteBasketProduct,
-        handleProductSelected,
-    } = useContext(OrderContext);
-    const { username } = useParams();
+  const {
+    username,
+    menu,
+    isModeAdmin,
+    handleDelete,
+    resetMenu,
+    productSelected,
+    setProductSelected,
+    handleAddToBasket,
+    handleDeleteBasketProduct,
+    handleProductSelected,
+  } = useContext(OrderContext)
+  // state
 
-    // state
+  // comportements (gestionnaires d'événement ou "event handlers")
+  const handleCardDelete = (event, idProductToDelete) => {
+    event.stopPropagation()
+    handleDelete(idProductToDelete, username)
+    handleDeleteBasketProduct(idProductToDelete, username)
+    idProductToDelete === productSelected.id && setProductSelected(EMPTY_PRODUCT)
+  }
 
-    // comportements (gestionnaires d'événement ou "event handlers")
-    const handleCardDelete = (event, idProductToDelete) => {
-        event.stopPropagation();
-        handleDelete(idProductToDelete);
-        handleDeleteBasketProduct(idProductToDelete);
-        idProductToDelete === productSelected.id &&
-            setProductSelected(EMPTY_PRODUCT);
-    };
+  const handleAddButton = (event, idProductToAdd) => {
+    event.stopPropagation()
+    handleAddToBasket(idProductToAdd, username)
+  }
 
-    const handleAddButton = (event, idProductToAdd) => {
-        event.stopPropagation();
-        handleAddToBasket(idProductToAdd);
-    };
+  // affichage
+  if (menu === undefined) return <Loader />
 
-    // const activateUser = async (username) => {
-    //     // const existingUser = await getUser(username);
-    //     // console.log("existingUser : ", existingUser.menu);
-    //     // return existingUser.menu;
-    //     return "bou";
-    // };
-    // const tab = activateUser(username);
+  if (isEmpty(menu)) {
+    if (!isModeAdmin) return <EmptyMenuClient />
+    return <EmptyMenuAdmin onReset={() => resetMenu(username)} />
+  }
 
-    // useEffect(async () => {
-    //     const menuReceived = await getUser(username);
-    //     console.log("menuReceived : ", menuReceived);
-    //     // setMenu(menuReceived);
-    // }, []);
-
-    // affichage
-    // if (isEmpty(getUser(username))) {
-    //     if (!isModeAdmin) return <EmptyMenuClient />;
-    //     return <EmptyMenuAdmin onReset={resetMenu} />;
-    // }
-
-    return (
-        <TransitionGroup component={MenuStyled} className={"transition-group"}>
-            {/* <MenuStyled className="menu"> */}
-            {menu.map(({ id, title, imageSource, price }) => {
-                return (
-                    <CSSTransition
-                        appear={true}
-                        classNames={"menu-animated"}
-                        key={id}
-                        timeout={150}
-                    >
-                        <Card
-                            key={id}
-                            title={title}
-                            imageSource={
-                                imageSource ? imageSource : IMAGE_COMING_SOON
-                            }
-                            leftDescription={formatPrice(price)}
-                            hasDeleteButton={isModeAdmin}
-                            onDelete={(event) => handleCardDelete(event, id)}
-                            onClick={
-                                isModeAdmin
-                                    ? () => handleProductSelected(id)
-                                    : null
-                            }
-                            isHoverable={isModeAdmin}
-                            isSelected={checkIfProductIsClicked(
-                                id,
-                                productSelected.id
-                            )}
-                            onAdd={(event) => handleAddButton(event, id)}
-                        />
-                    </CSSTransition>
-                );
-            })}
-            {/* </MenuStyled> */}
-        </TransitionGroup>
-    );
+  return (
+    <TransitionGroup component={MenuStyled} className="menu">
+      {menu.map(({ id, title, imageSource, price }) => {
+        return (
+          <CSSTransition classNames={"menu-animation"} key={id} timeout={300}>
+            <Card
+              title={title}
+              imageSource={imageSource ? imageSource : IMAGE_COMING_SOON}
+              leftDescription={formatPrice(price)}
+              hasDeleteButton={isModeAdmin}
+              onDelete={(event) => handleCardDelete(event, id)}
+              onClick={isModeAdmin ? () => handleProductSelected(id) : null}
+              isHoverable={isModeAdmin}
+              isSelected={checkIfProductIsClicked(id, productSelected.id)}
+              onAdd={(event) => handleAddButton(event, id)}
+            />
+          </CSSTransition>
+        )
+      })}
+    </TransitionGroup>
+  )
 }
 
 const MenuStyled = styled.div`
-    background: ${theme.colors.background_white};
-    display: grid;
-    grid-template-columns: repeat(3, 1fr);
-    /* grid-template-columns: repeat(auto-fit, minmax(300px, 1fr)); */
-    grid-row-gap: 60px;
-    padding: 50px 50px 150px;
-    justify-items: center;
-    box-shadow: 0px 8px 20px 8px rgba(0, 0, 0, 0.2) inset;
-    overflow-y: scroll;
+  background: ${theme.colors.background_white};
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
+  /* grid-template-columns: repeat(auto-fit, minmax(300px, 1fr)); */
+  grid-row-gap: 60px;
+  padding: 50px 50px 150px;
+  justify-items: center;
+  box-shadow: 0px 8px 20px 8px rgba(0, 0, 0, 0.2) inset;
+  overflow-y: scroll;
 
-    /* .menu-animated-appear {
-        opacity: 0%;
-    }
-    .menu-animated-appear-active {
-        opacity: 100%;
-        transition: 300ms;
-    } */
-
-    .menu-animated-enter {
-        transform: translateX(-100%);
-    }
-    .menu-animated-enter-active {
-        transform: translateX(0%);
-        transition: 75ms;
-    }
-    .menu-animated-enter-done {
-    }
-    .menu-animated-exit {
-        opacity: 100%;
-        transition: 75ms;
-    }
-
-    .menu-animated-exit-active {
-        opacity: 0%;
-        transition: 75ms;
-    }
-    .menu-animated-exit-done {
-    }
-`;
+  ${menuAnimation}
+`
