7fbe0926d3148c0f8bf4f8da38187ea6911933be
MaximeextremeBBB <maxime.guiraud.1990@gmail.com>
2024-05-22 16:35:53 +0200

    ✨ F15 : Form > Add ads and availability

--- src/components/pages/order/Main/MainRightSide/Admin/AdminPanel/Form.js
+++ src/components/pages/order/Main/MainRightSide/Admin/AdminPanel/Form.js
@@ -1,64 +1,152 @@
-import React from "react"
-import styled from "styled-components"
-import TextInput from "../../../../../../reusable-ui/TextInput"
-import ImagePreview from "./ImagePreview"
-import { getInputTextsConfig } from "./inputTextConfig"
+import React from "react";
+import styled from "styled-components/macro";
+import TextInput from "../../../../../../reusable-ui/TextInput";
+import ImagePreview from "./ImagePreview";
+import { getInputTextsConfig, getPrice } from "./inputTextConfig";
+import { GoMegaphone } from "react-icons/go";
+import { FiPackage } from "react-icons/fi";
+import { theme } from "../../../../../../../theme";
 
-const Form = React.forwardRef(({ product, onSubmit, onChange, onFocus, onBlur, children }, ref) => {
-  // state (vide)
+const Form = React.forwardRef(
+    ({ product, onSubmit, onChange, onFocus, onBlur, children }, ref) => {
+        // state (vide)
 
-  // comportements (vide)
+        // comportements (vide)
 
-  const inputTexts = getInputTextsConfig(product)
+        const inputTexts = getInputTextsConfig(product);
+        const priceValue = getPrice(product);
+        console.log("inputTexts : ", inputTexts);
+        console.log("inputTexts : ", inputTexts[0].Icon);
 
-  // affichage
-  return (
-    <FormStyled onSubmit={onSubmit}>
-      <ImagePreview imageSource={product.imageSource} title={product.title} />
-      <div className="input-fields">
-        {inputTexts.map((input) => (
-          <TextInput
-            {...input}
-            key={input.id}
-            onChange={onChange}
-            version="minimalist"
-            onFocus={onFocus}
-            onBlur={onBlur}
-            ref={ref && input.name === "title" ? ref : null}
-          />
-        ))}
-      </div>
-      <div className="form-footer">{children}</div>
-    </FormStyled>
-  )
-})
+        // affichage
+        return (
+            <FormStyled onSubmit={onSubmit}>
+                <ImagePreview
+                    imageSource={product.imageSource}
+                    title={product.title}
+                />
+                <div className="input-fields">
+                    {inputTexts.map((input) => (
+                        <TextInput
+                            {...input}
+                            key={input.id}
+                            onChange={onChange}
+                            version="minimalist"
+                            onFocus={onFocus}
+                            onBlur={onBlur}
+                            ref={ref && input.name === "title" ? ref : null}
+                        />
 
-export default Form
+                        // {input.name === "price" ? "bonjour" : ""}
+                    ))}
+                    <PriceStyled>
+                        <div>
+                            <TextInput
+                                {...inputTexts}
+                                name="price"
+                                placeholder="Prix"
+                                value={product.price ? product.price : ""}
+                                onChange={onChange}
+                                Icon={priceValue[0].Icon}
+                                version="minimalist"
+                                onFocus={onFocus}
+                                onBlur={onBlur}
+                                ref={
+                                    ref && inputTexts.name === "title"
+                                        ? ref
+                                        : null
+                                }
+                            />
+                        </div>
+                        <SelectStyled>
+                            <IconStyled>
+                                <FiPackage />
+                            </IconStyled>
+                            <div>
+                                <select name="available" id="available">
+                                    <option value="en-stock">En stock</option>
+                                    <option value="en-rupture">
+                                        En rupture
+                                    </option>
+                                </select>
+                            </div>
+                        </SelectStyled>
+                        <SelectStyled>
+                            <IconStyled>
+                                <GoMegaphone />
+                            </IconStyled>
+                            <div>
+                                <select name="ads " id="ads">
+                                    <option value="sans-pub">Sans pub</option>
+                                    <option value="avec-pub">Avec pub</option>
+                                </select>
+                            </div>
+                        </SelectStyled>
+                    </PriceStyled>
+                </div>
+
+                <div className="form-footer">{children}</div>
+            </FormStyled>
+        );
+    }
+);
+
+export default Form;
 
 const FormStyled = styled.form`
-  /* border: 2px solid black; */
-  display: grid;
-  grid-template-columns: 1fr 3fr;
-  grid-template-rows: repeat(4, 1fr);
-  height: 100%;
-  width: 70%;
-  grid-column-gap: 20px;
-  grid-row-gap: 8px;
-
-  .input-fields {
-    /* background: blue; */
-    grid-area: 1 / 2 / -2 / 3;
-
+    /* border: 2px solid black; */
     display: grid;
+    grid-template-columns: 1fr 3fr;
+    grid-template-rows: repeat(4, 1fr);
+    height: 100%;
+    width: 70%;
+    grid-column-gap: 20px;
     grid-row-gap: 8px;
-  }
 
-  .form-footer {
-    /* background: green; */
-    grid-area: 4 / -2 / -1 / -1;
-    display: flex;
+    .input-fields {
+        /* background: blue; */
+        grid-area: 1 / 2 / -2 / 3;
+
+        display: grid;
+        grid-row-gap: 8px;
+    }
+
+    .form-footer {
+        /* background: green; */
+        grid-area: 4 / -2 / -1 / -1;
+        display: flex;
+        align-items: center;
+        position: relative;
+        top: 3px;
+    }
+`;
+
+const PriceStyled = styled.div`
+    display: grid;
+    grid-template-columns: 33.33% 33.33% 33.33%;
+    /* display: flex; */
     align-items: center;
-    position: relative;
-    top: 3px;
-  }
-`
+`;
+
+const SelectStyled = styled.div`
+    display: grid;
+    grid-template-columns: 1fr 1fr;
+    background-color: ${theme.colors.background_white};
+    margin-left: 10px;
+    select {
+        border: none;
+        font-size: ${theme.fonts.size.SM};
+        background-color: ${theme.colors.background_white};
+
+        /* width: 100%; */
+
+        &::placeholder {
+            color: ${theme.colors.greyMedium};
+        }
+    }
+`;
+
+const IconStyled = styled.div`
+    display: flex;
+    padding: 10px;
+`;
