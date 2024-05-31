ee415ba7b6c6d1ce8a1954bb612ad77c153f1314
MaximeextremeBBB <maxime.guiraud.1990@gmail.com>
2024-05-31 17:22:38 +0200

    correct bug in package.json

--- src/components/pages/order/Main/MainRightSide/Menu/Menu.js
+++ src/components/pages/order/Main/MainRightSide/Menu/Menu.js
@@ -7,11 +7,13 @@ import Card from "../../../../../reusable-ui/Card"
 import EmptyMenuAdmin from "./EmptyMenuAdmin"
 import EmptyMenuClient from "./EmptyMenuClient"
 import { checkIfProductIsClicked } from "./helper"
-import { EMPTY_PRODUCT, IMAGE_COMING_SOON } from "../../../../../../enums/product"
+import { EMPTY_PRODUCT, IMAGE_COMING_SOON, IMAGE_NO_STOCK } from "../../../../../../enums/product"
 import { isEmpty } from "../../../../../../utils/array"
 import Loader from "./Loader"
 import { CSSTransition, TransitionGroup } from "react-transition-group"
 import { menuAnimation } from "../../../../../../theme/animations"
+import { convertStringToBoolean } from "../../../../../../utils/string"
+import RibbonAnimated, { ribbonAnimation } from "./RibbonAnimated"
 
 export default function Menu() {
   const {
@@ -41,6 +43,8 @@ export default function Menu() {
     handleAddToBasket(idProductToAdd, username)
   }
 
+  let cardContainerClassName = isModeAdmin ? "card-container is-hoverable" : "card-container"
+
   // affichage
   if (menu === undefined) return <Loader />
 
@@ -51,20 +55,25 @@ export default function Menu() {
 
   return (
     <TransitionGroup component={MenuStyled} className="menu">
-      {menu.map(({ id, title, imageSource, price }) => {
+      {menu.map(({ id, title, imageSource, price, isAvailable, isPublicised }) => {
         return (
           <CSSTransition classNames={"menu-animation"} key={id} timeout={300}>
-            <Card
-              title={title}
-              imageSource={imageSource ? imageSource : IMAGE_COMING_SOON}
-              leftDescription={formatPrice(price)}
-              hasDeleteButton={isModeAdmin}
-              onDelete={(event) => handleCardDelete(event, id)}
-              onClick={isModeAdmin ? () => handleProductSelected(id) : null}
-              isHoverable={isModeAdmin}
-              isSelected={checkIfProductIsClicked(id, productSelected.id)}
-              onAdd={(event) => handleAddButton(event, id)}
-            />
+            <div className={cardContainerClassName}>
+              {convertStringToBoolean(isPublicised) && <RibbonAnimated />}
+              <Card
+                title={title}
+                imageSource={imageSource ? imageSource : IMAGE_COMING_SOON}
+                leftDescription={formatPrice(price)}
+                hasDeleteButton={isModeAdmin}
+                onDelete={(event) => handleCardDelete(event, id)}
+                onClick={isModeAdmin ? () => handleProductSelected(id) : null}
+                isHoverable={isModeAdmin}
+                isSelected={checkIfProductIsClicked(id, productSelected.id)}
+                onAdd={(event) => handleAddButton(event, id)}
+                overlapImageSource={IMAGE_NO_STOCK}
+                isOverlapImageVisible={convertStringToBoolean(isAvailable) === false}
+              />
+            </div>
           </CSSTransition>
         )
       })}
@@ -84,4 +93,23 @@ const MenuStyled = styled.div`
   overflow-y: scroll;
 
   ${menuAnimation}
+
+  .card-container {
+    position: relative;
+    height: 330px; // pour éviter une zone de click verticale bizarre qu'on voit qu'au pointeur de l'outil inspect du navigateur
+    border-radius: ${theme.borderRadius.extraRound};
+
+    &.is-hoverable {
+      :hover {
+        /* border: 1px solid red; */
+        transform: scale(1.05);
+        transition: ease-out 0.4s;
+      }
+    }
+  }
+
+  .ribbon {
+    z-index: 2;
+  }
+  ${ribbonAnimation}
 `
